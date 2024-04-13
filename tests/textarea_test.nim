0b75222e65f7604910a1c7331733ade358dbbd9e
jaar23 <yongjia.ibm@gmail.com>
2024-04-13 13:15:29 +0800

    update textarea test

--- tests/textarea_test.nim
+++ tests/textarea_test.nim
@@ -2,6 +2,11 @@ import tui_widget
 
 var textarea = newTextArea(1, 1, consoleWidth(), 10, title="textarea", statusbar=true, enableViMode=true, cursorStyle=Ibeam)
 
+let clearEv = proc (t: ref TextArea, args: varargs[string]) =
+  t.value = ""
+
+textarea.on("clear", clearEv)
+
 var label = newLabel(1, 11, 40, 11, text="Welcome, Textarea")
 
 var textarea2 = newTextArea(1, 12, consoleWidth(), 22, title="textarea 2", statusbar=true)
